70387d8bff1651bd14895fdf95ab6512e118e7e8
unknown <chooliangyi6@gmail.com>
2024-05-07 23:16:27 +0800

    test commit

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const AppLayout = () => {
+  
   return (
     <div className="flex flex-col min-h-screen layout">
       <Navbar />
